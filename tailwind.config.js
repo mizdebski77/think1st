8503032bc0e1f7fec24a0275da7e21367e8667bb
Marcin Izdebski <mizdebski123@gmail.com>
2025-02-13 15:52:58 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,tsx,ts}",
+    "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
